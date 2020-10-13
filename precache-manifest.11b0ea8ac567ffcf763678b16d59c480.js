6c1477a0cdc94fd4b53780b5b98444458d9c57e3
David Nyman <davenyman@gmail.com>
2020-10-12 23:09:57 -0400

    Updates

---
+++ precache-manifest.11b0ea8ac567ffcf763678b16d59c480.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "148b22660899932b8fc546ec17372695",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c92ee14d6383c6a2281",
+    "url": "/static/css/main.8b803b11.chunk.css"
+  },
+  {
+    "revision": "d19cf69d3edbc0debc99",
+    "url": "/static/js/2.6b7aafd9.chunk.js"
+  },
+  {
+    "revision": "d2907550b0749ad10542a424f45133ae",
+    "url": "/static/js/2.6b7aafd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c92ee14d6383c6a2281",
+    "url": "/static/js/main.d2169011.chunk.js"
+  },
+  {
+    "revision": "60907134c53065513b67",
+    "url": "/static/js/runtime-main.008182a4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
